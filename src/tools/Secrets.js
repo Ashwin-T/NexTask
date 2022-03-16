4c2f5f8df0e574997cb6a7c29ec1cb07d50adb5c
zphyrj <46908127+zphyrj@users.noreply.github.com>
2022-03-15 22:51:49 -0700

    disabled format on save

---
+++ src/tools/Secrets.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBidnICqOFYJgBSVII2jclG9k79DQvz2zU",
+  authDomain: "nextask-app.firebaseapp.com",
+  projectId: "nextask-app",
+  storageBucket: "nextask-app.appspot.com",
+  messagingSenderId: "565857426945",
+  appId: "1:565857426945:web:22490556d613b47ace2a00",
+  measurementId: "G-1QYMCD6J9W",
+}
+
+export default firebaseConfig
